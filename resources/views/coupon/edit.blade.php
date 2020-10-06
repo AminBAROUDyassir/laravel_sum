be42f95a6e0a56014e848aafaf66d670784516ca
Brahim LAMRI <brahim.lamri@yatechnologies.com>
2020-10-06 15:05:48 +0100

    1

--- resources/views/coupon/edit.blade.php
+++ resources/views/coupon/edit.blade.php
@@ -6,7 +6,7 @@
         <div class="card">
             <div class="card-body" style="text-align:center;">
       
-    <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($coupon->code, 'C39+',2,50) }}" alt="barcode"   />
+    <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($coupon->code, 'C39',2,50) }}" alt="barcode"   />
             </div>
         </div>
     </div>
