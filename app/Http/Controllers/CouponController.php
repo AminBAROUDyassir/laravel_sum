b0c0c1728af78d82e277687b907f4d724a4fcf79
Brahim LAMRI <brahim.lamri@yatechnologies.com>
2020-10-06 14:18:39 +0100

    1

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -220,6 +220,7 @@ class CouponController extends Controller
             $coupon = Coupon::where('id', $request->coupon_id)->first();
 
             info("coupon _id  :" . $request->coupon_id);
+            info("coupon _id  :" . $request->all());
             if ($coupon == null) {
                 $response_array = ['success' => false, 'error_messages' => 'no data', 'error_code' => "404"];
 
