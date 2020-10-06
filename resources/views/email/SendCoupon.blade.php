c5e2996ee1a824145071d3af55c46c8e0f22666e
Brahim LAMRI <brahim.lamri@yatechnologies.com>
2020-10-06 16:23:33 +0100

    1

--- resources/views/email/SendCoupon.blade.php
+++ resources/views/email/SendCoupon.blade.php
@@ -9,7 +9,7 @@
     <p>{{ $details['picture'] }}</p>
 
     <img src="{{ $message->embed(public_path() . '/images/50x40.png') }}" alt="" />
-    <img src="{{ $message->embed("data:image/png;base64,".DNS1D::getBarcodePNG($details['barcode'], 'C39+' ,1,50) ) }}" alt="" />
+    <img src="{{ $message->embed("data:image/png;base64,".DNS1D::getBarcodePNG($details['barcode'], 'C39' ,1,50) ) }}" alt="" />
    
     <p>Thank you</p>
 </body>
